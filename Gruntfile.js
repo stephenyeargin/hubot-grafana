4d91c74367fb77a822dd32d2c40b2bba3c006deb
Stephen Yeargin <stephen.yeargin@gmail.com>
2017-11-18 23:44:27 -0600

    Sync package-lock.json version with package.json
    This got out of sync because the `Gruntfile.js` was missing a definition
    to updated it as well on release. Also update a handful of dependencies.

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,7 +19,8 @@ module.exports = function (grunt) {
     release: {
       options: {
         tagName: 'v<%= version %>',
-        commitMessage: 'Prepared to release <%= version %>.'
+        commitMessage: 'Prepared to release <%= version %>.',
+        additionalFiles: ['package-lock.json']
       }
     },
     watch: {
