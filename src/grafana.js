889516ff33359f2092344073d0f13a4eb3af7e69
Kees C. Bakker <k.bakker@hostedsolutions.nu>
2024-04-22 06:49:21 +0000

    Make sure the env variable is an integer

--- src/grafana.js
+++ src/grafana.js
@@ -91,8 +91,8 @@ module.exports = (robot) => {
     let apiPanelId = false;
     let pname = false;
     const query = {
-      width: process.env.HUBOT_GRAFANA_DEFAULT_WIDTH || 1000,
-      height: process.env.HUBOT_GRAFANA_DEFAULT_HEIGHT || 500,
+      width: parseInt(process.env.HUBOT_GRAFANA_DEFAULT_WIDTH) || 1000,
+      height: parseInt(process.env.HUBOT_GRAFANA_DEFAULT_HEIGHT) || 500,
       tz: process.env.HUBOT_GRAFANA_DEFAULT_TIME_ZONE || '',
       orgId: process.env.HUBOT_GRAFANA_ORG_ID || '',
       apiEndpoint: process.env.HUBOT_GRAFANA_API_ENDPOINT || 'd-solo',
