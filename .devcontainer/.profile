55b2c3f80f0ef8e05814167637c4278fe0c2c01e
Kees C. Bakker <k.bakker@hostedsolutions.nu>
2024-12-11 09:56:12 +0000

    fix: update dev container
    - Ship new NPM version.
    - Make trusting the git repo part of the profile startup.

---
+++ .devcontainer/.profile
@@ -0,0 +1,4 @@
+# trust the repo
+# fixes:
+# - fatal: detected dubious ownership in repository at '/workspaces/bot-zero'.
+git config --global --add safe.directory "$PWD"
--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-# trust the repo
-# fixes:
-# - fatal: detected dubious ownership in repository at '/workspaces/bot-zero'.
-git config --global --add safe.directory "$PWD"
-
 # install NPM packages
 echo ""
 echo "Installing packages..."
